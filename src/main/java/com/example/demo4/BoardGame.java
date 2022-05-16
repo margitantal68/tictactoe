885031708d068c190e80d63816ca552dbc743b64
Margit Antal <margitantal68@gmail.com>
2022-05-16 16:12:25 +0300

    event handling updated

--- src/main/java/com/example/demo4/BoardGame.java
+++ src/main/java/com/example/demo4/BoardGame.java
@@ -20,38 +20,40 @@ public class BoardGame extends GridPane {
         createView();
     }
 
-    public void createView(){
+    private void createView(){
         for(int i=0; i<size; ++i){
             for(int j=0; j<size; ++j){
                 boardView[i][j] = new Field(i, j);
-
                 boardView[i][j].setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent actionEvent) {
-                        Field source = (Field) actionEvent.getSource();
-                        int row = source.getRow();
-                        int column = source.getColumn();
-                        System.out.printf("%d,%d\n", row, column);
-                        if( board.isEmpty(row, column)){
-                            board.setPosition(row, column, currentPlayer);
-                            boardView[row][column].setText("" + currentPlayer);
-                            if( board.isGameOver(row, column, currentPlayer) ){
-                                showGameOver("Player " + currentPlayer + " is the winner!");
-                                exitGame();
-                            }
-                            if( board.isDraw() ){
-                                showGameOver("Draw");
-                                exitGame();
-                            }
-                            currentPlayer = 1 -currentPlayer;
-                        }
-
+                        handleButtonPressed(actionEvent);
                     }
                 });
                 this.add(boardView[i][j], j, i);
             }
         }
 
+    }
+
+    private void handleButtonPressed(ActionEvent event){
+        Field source = (Field) event.getSource();
+        int row = source.getRow();
+        int column = source.getColumn();
+        System.out.printf("%d,%d\n", row, column);
+        if( board.isEmpty(row, column)){
+            board.setPosition(row, column, currentPlayer);
+            boardView[row][column].setText("" + currentPlayer);
+            if( board.isGameOver(row, column, currentPlayer) ){
+                showGameOver("Player " + currentPlayer + " is the winner!");
+                exitGame();
+            }
+            if( board.isDraw() ){
+                showGameOver("Draw");
+                exitGame();
+            }
+            currentPlayer = 1 -currentPlayer;
+        }
     }
 
     private void showGameOver(String message){
